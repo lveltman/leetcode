46d011e16aa4a4f47d6dd09c269b104b52fe2a35
⚡ Darth Lina ⚡ <45791426+lveltman@users.noreply.github.com>
2022-02-27 19:57:58 +0300

    Update 217_contains_duplicate.cpp

--- DataStructure1/217_contains_duplicate.cpp
+++ DataStructure1/217_contains_duplicate.cpp
@@ -1,33 +1,33 @@
-////
-////  main.cpp
-////  leetcode
-////
-////  Created by Lina Veltman on 17.02.2022.
-////
 //
-//#include <iostream>
-//#include <vector>
-//#include <unordered_set>
-//using namespace std;
+//  main.cpp
+//  leetcode
 //
-//class Solution {
-//public:
-//    bool containsDuplicate(vector<int>& nums) {
-//        unordered_set<int> duplicates;
-//        for (const auto& n : nums) {
-//            if (duplicates.count(n))
-//                return true;
-//            duplicates.insert(n);
-//        }
-//        return false;
-//    }
-//};
+//  Created by Lina Veltman on 17.02.2022.
 //
-//int main(int argc, const char * argv[]) {
-//    // insert code here...
-//    vector<int> nums = {1,2,3,4};
-//    Solution sol;
-//    bool ans = sol.containsDuplicate(nums);
-//    cout << ans << endl;
-//    return 0;
-//}
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+   bool containsDuplicate(vector<int>& nums) {
+       unordered_set<int> duplicates;
+       for (const auto& n : nums) {
+           if (duplicates.count(n))
+               return true;
+           duplicates.insert(n);
+       }
+       return false;
+   }
+};
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   vector<int> nums = {1,2,3,4};
+   Solution sol;
+   bool ans = sol.containsDuplicate(nums);
+   cout << ans << endl;
+   return 0;
+}
