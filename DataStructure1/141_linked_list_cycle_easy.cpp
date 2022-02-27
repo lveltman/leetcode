b0302298d3c97053c343f882d726fb564f8793a4
⚡ Darth Lina ⚡ <45791426+lveltman@users.noreply.github.com>
2022-02-27 19:57:36 +0300

    Update 141_linked_list_cycle_easy.cpp

--- DataStructure1/141_linked_list_cycle_easy.cpp
+++ DataStructure1/141_linked_list_cycle_easy.cpp
@@ -1,48 +1,48 @@
-////
-////  141_linked_list_cycle_easy.cpp
-////  leetcode
-////
-////  Created by Lina Veltman on 17.02.2022.
-////
 //
-//#include <stdio.h>
-//#include <vector>
-//#include <unordered_set>
-//#include <memory>
+//  141_linked_list_cycle_easy.cpp
+//  leetcode
 //
-//using namespace std;
+//  Created by Lina Veltman on 17.02.2022.
 //
-////  Definition for singly-linked list.
-//struct ListNode {
-//  int val;
-//  ListNode *next;
-//  ListNode(int x) : val(x), next(NULL) {}
-//};
-// 
-//class Solution {
-//public:
-////    bool hasCycle(ListNode *head) {
-////        unordered_set<ListNode*> unique;
-////
-////        while (head != nullptr) {
-////            if (unique.count(head))
-////                return true;
-////            unique.insert(head);
-////            head = head->next;
-////        }
-////        return false;
-////    }
+
+#include <stdio.h>
+#include <vector>
+#include <unordered_set>
+#include <memory>
+
+using namespace std;
+
+//  Definition for singly-linked list.
+struct ListNode {
+ int val;
+ ListNode *next;
+ ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
 //    bool hasCycle(ListNode *head) {
-//        ListNode* node = head;
-//        ListNode* nextNode = head;
-//        while (nextNode && nextNode->next) {
-//            node = node->next;
-//            nextNode = nextNode->next->next;
-//            if (node == nextNode)
+//        unordered_set<ListNode*> unique;
+//
+//        while (head != nullptr) {
+//            if (unique.count(head))
 //                return true;
+//            unique.insert(head);
+//            head = head->next;
 //        }
 //        return false;
 //    }
-//};
-//
-//
+   bool hasCycle(ListNode *head) {
+       ListNode* node = head;
+       ListNode* nextNode = head;
+       while (nextNode && nextNode->next) {
+           node = node->next;
+           nextNode = nextNode->next->next;
+           if (node == nextNode)
+               return true;
+       }
+       return false;
+   }
+};
+
+
